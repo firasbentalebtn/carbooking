222b307d8f14b6cfccb707b3701e5ae452e570a2
firas Bentaleb <cyber@firass-Air.home>
2025-03-08 02:08:57 +0100

    first commit

---
+++ user/app/index.tsx
@@ -0,0 +1,9 @@
+import { Redirect } from "expo-router";
+import { useState } from "react";
+
+export default function index() {
+  const [isLoggedIn, setisLoggedIn] = useState(false);
+    return (
+    <Redirect href={!isLoggedIn ? "/(routes)/onboarding" : "/(tabs)/home"} />
+  );
+}
