8c08114893e53991e30d02c54ed99085e12fef67
firas Bentaleb <cyber@firass-Air.home>
2025-03-09 23:41:15 +0100

    otp codes sent succesffuly and app connected to backend server

--- server/server.ts
+++ server/server.ts
@@ -1,9 +1,9 @@
 import { Server } from './node_modules/@types/connect/index.d';
 import http from 'http';
 import {app} from './app';
-
+import cors from 'cors';
 const server = http.createServer(app);
-
+app.use(cors({ origin: '*' }));
 
 //create server
 server.listen(process.env.PORT, () => {
